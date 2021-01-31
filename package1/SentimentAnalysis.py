8cab5d3de02b39e5de05dd05b383d3be947e1e90
garnet-k <garnet0609.98@gmail.com>
2021-02-01 07:27:14 +0900

    辞書の更新に合わせて変更

--- package1/SentimentAnalysis.py
+++ package1/SentimentAnalysis.py
@@ -9,7 +9,7 @@ df_dic = pd.read_csv('modelData/polar_dictionary.csv') # 極性辞書
 
 def get_scoreY(word_list): # 感情分析のスコアを返す関数
     y = 0
-    y_list = np.array([-9.622200773328238, 2.095296998796651]) # 感情分析のスコアの最大値と最小値
+    y_list = np.array([-0.14892710197126388, 0.4472879118982551]) # 感情分析のスコアの最大値と最小値
     if len(word_list) == 0:
         return 0
     for text in word_list:
@@ -29,4 +29,11 @@ def get_scoreY(word_list): # 感情分析のスコアを返す関数
         return 0.0
     y_list = np.append(y_list, [y])
     y_std = (y - y_list.min()) / (y_list.max() - y_list.min())
+    
+    print("len", len(word_list))
+    print(" y ", y)
+    print("y_std", y_std)
+    print("min", y_list.min())
+    print("max", y_list.max())
+    
     return y_std * 2 - 1
